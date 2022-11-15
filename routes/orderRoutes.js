319f546d52550865db6a749b3eff5973b3f64c8f
nidhi <nidhi7485@gmail.com>
2022-11-15 10:30:54 +0530

    order routes completed

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -6,3 +6,25 @@ const {
   authenticateUser,
   authorizePermission,
 } = require('../middleware/authentication')
+
+const {
+  getAllOrder,
+  getSingleOrder,
+  getCurrentUserOrder,
+  createOrder,
+  updateOrder,
+} = require('../controllers/orderController')
+
+router
+  .route('/')
+  .post(authenticateUser, createOrder)
+  .get(authenticateUser, authorizePermission('admin'), getAllOrder)
+
+router.route('/showAllMyOrder').get(authenticateUser, getCurrentUserOrder)
+
+router
+  .route('/:id')
+  .get(authenticateUser, getSingleOrder)
+  .patch(authenticateUser, updateOrder)
+
+module.exports = router
